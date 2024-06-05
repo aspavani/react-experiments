af317d85230a11443d34096582e441cd93433a8a
Santhoshi Pavani A <santhoshipavani-all@pluralsight.com>
2024-06-05 17:19:57 +0530

    Refactor class components( in ToDoList.js) to functional components (ToDoList1.js)

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,13 +1,18 @@
 import Counter from './Counter';
 import SignUpForm from './SignUpForm.js';
+import ToDo from './Todo.js';
+import ToDoList1 from './ToDoList1.js';
+
 function App() {
   return (
     <>
       {/* <h1> Counter Component 1 </h1>
       <Counter initialCount={5} > </Counter>
-      <h1><br /> Counter Component 2</h1>
-      <Counter initialCount={6} > </Counter> */}
-      <SignUpForm />
+      <h1><br /> Counter Component 2</h1>*/}
+      {/* <Counter initialCount={6} > </Counter>   */}
+      {/* <SignUpForm /> */}
+      <ToDoList1 />
+      {/* <PavaniToDoList /> */}
     </>
   );
     
---
+++ counter-app/src/ToDoList1.js
@@ -0,0 +1,66 @@
+// import { Component } from 'react'
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import ToDo from './Todo'
+
+function ToDoList1() {
+    
+    const itasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    const [tasks, setTasks] = useState(itasks);
+    const [input, setInput] = useState('');
+
+    useEffect(() => {
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    });
+    const handleAdd = () => {
+        console.log(tasks);
+        if (!input) return;
+
+        setTasks([...tasks, { taskName: input, completed: false }])
+    }
+
+    const handleRemove = (index) => {
+        const newTasks = [...tasks];
+
+        newTasks.splice(index, 1);
+
+        setTasks(newTasks);
+    }
+
+    const handleChange = (event) => {
+        setInput(event.target.value);
+    }
+
+    const handleToggle = (index) => {
+        const newTasks = [...tasks];
+
+        newTasks[index].completed = !newTasks[index].completed;
+
+        setTasks(newTasks);
+    }
+
+    
+    return (
+        <div>
+            <input value={input} onChange={handleChange} />
+            <button onClick={handleAdd}>add</button>
+
+            <ul>
+                {tasks.map((task, index) => {
+                    return (
+                        <ToDo 
+                            key={index}
+                            index={index}
+                            task={task}
+                            handleToggle={handleToggle}
+                            handleRemove={handleRemove}
+                        />
+                    )
+                })}
+            </ul>
+        </div>
+    )
+
+}
+
+export default ToDoList1
