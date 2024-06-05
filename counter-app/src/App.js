500c45c05e235830a91c742836895b56b076054b
Santhoshi Pavani A <santhoshipavani-all@pluralsight.com>
2024-06-05 17:27:59 +0530

    Added useEffect() hook to the counter component

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -6,12 +6,12 @@ import ToDoList1 from './ToDoList1.js';
 function App() {
   return (
     <>
-      {/* <h1> Counter Component 1 </h1>
+       <h1> Counter Component 1 </h1>
       <Counter initialCount={5} > </Counter>
-      <h1><br /> Counter Component 2</h1>*/}
-      {/* <Counter initialCount={6} > </Counter>   */}
+      {/* <h1><br /> Counter Component 2</h1>*/ }
+      {/* <Counter initialCount={6} > </Counter>    */}
       {/* <SignUpForm /> */}
-      <ToDoList1 />
+      {/* <ToDoList1 /> */}
       {/* <PavaniToDoList /> */}
     </>
   );
--- counter-app/src/Counter.jsx
+++ counter-app/src/Counter.jsx
@@ -1,27 +1,35 @@
-import React, {Component, useState} from "react";
+import React, {Component, useEffect, useState} from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 
 
 function Counter(props) {
 
     let {initialCount} = props;
-    
     initialCount = ((initialCount === undefined) ? 0 : initialCount);
+    
     let [count, setCount] = useState(initialCount);
-
+    useEffect(() => {
+        const newCount = Number(localStorage.getItem('count'));
+        if (newCount) {
+            setCount(newCount);
+        }
+    }, []);
     const incrementCount = () => {
         setCount(++count > 20 ? 20 : count);
+        localStorage.setItem('count', count);
     }
 
     const decrementCount = () => {
         setCount(--count < 0 ? 0 : count);
+        localStorage.setItem('count', count);
     };
 
     const resetCount = () => {
         setCount(initialCount);
+        localStorage.setItem('count', initialCount);
     }
 
-    return (<>
+    return (<div className="container">
     
     
     <button className="btn btn-outline-primary" type="button" id="decrement" onClick={decrementCount}>Decrement</button>
@@ -29,7 +37,7 @@ function Counter(props) {
     <button className="btn btn-outline-primary mt" type="button" id="increment" onClick={incrementCount}>Increment</button>
     <br />
     <button className="btn btn-outline-primary" type="reset" id="reset" onClick={resetCount} style={{display:(count == initialCount ? "none" : "")}}>Reset</button>
-    </>);
+    </div>);
 
     
 
